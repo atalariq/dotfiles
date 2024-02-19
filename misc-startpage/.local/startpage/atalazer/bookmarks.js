9319d7c9ee64184fc20633c70e72a605639e56aa
Atalariq Barra Hadinugraha <atalariq26@gmail.com>
2024-02-19 13:31:46 +0700

    chore(misc): add scripts and browser startpage

---
+++ misc-startpage/.local/startpage/atalazer/bookmarks.js
@@ -0,0 +1,44 @@
+// Note: having length != 4 will mess with layout based on how the site is styled
+const bookmarks = [
+  {
+    title: "Work",
+    links: [
+      { name: "To-Do List", url: "https://tasksboard.com/app" },
+      { name: "Element", url: "https://app.element.io" },
+      { name: "Flowmodor", url: "https://app.flowmodor.com/"},
+      { name: "Gmail", url: "https://inbox.google.com" },
+      { name: "Simplenote", url: "https://app.simplenote.com" },
+      { name: "WordPress", url: "https://wordpress.com/" },
+    ],
+  },
+  {
+    title: "Study",
+    links: [
+      { name: "Brainscape", url: "https://www.brainscape.com/" },
+      { name: "CK-12", url: "https://www.ck12.org/" },
+      { name: "Khan Academy", url: "https://www.khanacademy.org" },
+      { name: "Rubberduck", url: "https://www.rubberduck.gg/learn"},
+      { name: "Quizlet", url: "https://quizlet.com/latest" },
+    ],
+  },
+  {
+    title: "Fun",
+    links: [
+      { name: "Omnivore", url: "https://omnivore.app/home" },
+      { name: "Quora", url: "https://quora.com" },
+      { name: "Quora ID", url: "https://id.quora.com" },
+      { name: "YouTube", url: "https://youtube.com" },
+    ],
+  },
+  {
+    title: "Code",
+    links: [
+      { name: "FEM", url: "https://frontendmasters.com/dashboard/" },
+      { name: "GitHub", url: "https://github.com" },
+      { name: "Roadmap", url: "https://roadmap.sh/" },
+      { name: "TLX TOKI", url: "https://tlx.toki.id/" },
+      { name: "TOP", url: "https://www.theodinproject.com/dashboard" },
+      { name: "USACO", url: "https://usaco.guide/" },
+    ],
+  },
+];
--- setup.sh
+++ setup.sh
@@ -8,6 +8,8 @@ stow --dir $SOURCE --target $TARGET -R app-gui
 stow --dir $SOURCE --target $TARGET -R app-nvim
 stow --dir $SOURCE --target $TARGET -R system
 stow --dir $SOURCE --target $TARGET -R system-archlinux
+stow --dir $SOURCE --target $TARGET -R misc-script
+stow --dir $SOURCE --target $TARGET -R misc-startpage
 stow --dir $SOURCE --target $TARGET -R wm-hyprland
 
 
