325b6bc544998cab174b04f48ca2f3013122dfe2
Atalariq Barra Hadinugraha <atalariq.dev@outlook.com>
2026-05-07 06:53:50 +0700

    feat(mango): update tags.conf, binds, config, add macro.sh

---
+++ desktop/mango/.config/mango/script/macro.sh
@@ -0,0 +1,15 @@
+#!/bin/sh
+
+# wlrctl pointer move 0 120
+# wlrctl pointer click
+# sleep 0.5 && wtype "DD_11_"
+
+# sleep 7 && wlrctl pointer move 0 -120
+# wlrctl pointer click
+
+# wlrctl pointer scroll 20 0
+
+# wlrctl keyboard type A modifiers SUPER
+
+# # Toast
+# qs -c noctalia-shell ipc call toast send '{"title": "Macro", "body": "Executed", "icon": "media-record", "duration": 500}'
