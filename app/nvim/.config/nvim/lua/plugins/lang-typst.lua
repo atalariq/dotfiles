f6027c2d5b9952ab16d98a62ad719feb08384b8c
Atalariq Barra Hadinugraha <atalariq.dev@outlook.com>
2026-05-19 22:13:52 +0700

    refactor(nvim): consolidate typst plugins into lang-typst
    Move typst-preview config from typst.lua to lang-typst.lua
    for consistent naming with lang-markdown.

--- app/nvim/.config/nvim/lua/plugins/lang-typst.lua
+++ app/nvim/.config/nvim/lua/plugins/lang-typst.lua
@@ -8,11 +8,15 @@ return {
     opts = {
       debug = false,
       -- open_cmd = "firefox %s -P previewer --class previewer",
-      -- open_cmd = "/opt/brave-bin/brave --app=%s --profile-directory=Default --app-id=previewer",
+      open_cmd = "/opt/brave-bin/brave --app=%s --profile-directory=Default --app-id=previewer",
       port = 0,
       host = "127.0.0.1",
       invert_colors = "never",
       follow_cursor = true,
+      dependencies_bin = {
+        tinymist = "/usr/bin/tinymist",
+        websocat = "/usr/bin/websocat",
+      },
     },
   },
 }
