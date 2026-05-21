8af56edf9dfe7b19ca404a8b1fcbdb2b23df3b9b
Atalariq Barra Hadinugraha <atalariq.dev@outlook.com>
2026-05-21 19:26:17 +0700

    chore(fish): remove unused conf, add nvim alias

--- app/fish/.config/fish/conf.d/me-as-software-engineering-student.fish
+++
@@ -1,4 +0,0 @@
-# Persoanl Stuff
-
-alias new-laprak ~/.agents/skills/typst-lab-report/scripts/new-laporan.sh
-
---
+++ app/fish/.config/fish/conf.d/nvim.fish
@@ -0,0 +1,4 @@
+alias nvim-lazyvim "env NVIM_APPNAME=nvim-lazyvim nvim"
+alias nvim-minimal "env NVIM_APPNAME=nvim-minimal nvim"
+
+set -gx NVIM_APPNAME nvim
